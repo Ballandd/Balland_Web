d077f9e1e15b6bbfbb2a73c74974bf5771b9bf0f
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2023-03-15 17:49:41 +0900

    Design(bigground): 대운동장 디자인 수정

--- balland/pages/club/index.js
+++ balland/pages/club/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-
+import styles from "../../styles/Bigground.module.css"
 export default function Club() {
 
   return (
@@ -10,10 +10,291 @@ export default function Club() {
       </Head>
 
 
-      {/* content goes here */}
-      <div className="py-32 text-center">
-        <div className="text-4xl font-extrabold">Club main page</div>
+      {/* content goes here e1_2*/}
+      <div className = {styles.e1_2}>
+  <div className={styles.e4_1061}>
+    <div className={styles.e4_1062}></div>
+    <div className={styles.e4_1063}>
+      <div className={styles.e4_1064}>
+        <div className={styles.e4_1065}></div>
+        <div className={styles.e4_1066}></div>
       </div>
+    </div>
+    <div className={styles.e4_1067}>
+      <div className={styles.e4_1068}>
+        <div className={styles.e4_1069}></div>
+        <div className={styles.e4_1070}></div>
+      </div>
+    </div>
+    <div className={styles.e4_1071}>
+      <div className={styles.e4_1072}></div>
+      <div className={styles.e4_1073}></div>
+      <div className={styles.e4_1074}></div>
+    </div>
+  </div>
+  <div className={styles.e4_1075}>
+    <div className={styles.e4_1076}></div><span className={styles.e4_1077}>강변리틀야구장</span>
+    <div className={styles.e4_1078}>
+      <div className={styles.e4_1079}>
+        <div className={styles.e4_1080}>
+          <div className={styles.e4_1081}></div>
+          <div className={styles.e4_1082}></div>
+        </div>
+      </div><span  className={styles.e4_1083}>053-803-8323</span>
+    </div>
+    <div className={styles.e4_1084}>
+      <div className={styles.e4_1085}>
+        <div className={styles.e4_1086}>
+          <div className={styles.e4_1087}></div>
+          <div className={styles.e4_1088}></div>
+        </div>
+      </div><span  className={styles.e4_1089}>강변리틀야구장</span>
+    </div>
+    <div className={styles.e4_1090}>
+      <div className={styles.e4_1091}>
+        <div className={styles.e4_1092}>
+          <div className={styles.e4_1093}></div>
+          <div className={styles.e4_1094}></div>
+        </div>
+      </div><span className={styles.e4_1095}>대구광역시 북구 서변동 1506</span>
+    </div>
+    <div className={styles.e4_1096}></div>
+  </div>
+  {/* <div className={styles.e4_1097}>
+    <div className="e4_1098"></div><span  className="e4_1099">예약 날짜 선택</span>
+    <div className=e4_1100>
+      <div className=e4_1101><span  className="e4_1102">SUN</span><span  className="e4_1103">MON</span><span  className="e4_1104">TUE</span><span  className="e4_1105">WED</span><span  className="e4_1106">THU</span><span  className="e4_1107">FRI</span><span  className="e4_1108">SAT</span>
+        <div className="e4_1109"></div>
+        <div className="e4_1110"></div>
+      </div>
+      <div className=e4_1111>
+        <div className="e4_1112"></div>
+        <div className=e4_1113>
+          <div className="e4_1114"></div>
+          <div className="e4_1115"></div>
+          <div className="e4_1116"></div>
+          <div className="e4_1117"></div>
+          <div className="e4_1118"></div>
+          <div className="e4_1119"></div>
+          <div className="e4_1120"></div>
+        </div>
+        <div className="e4_1121"></div><span  className="e4_1122">26</span><span  className="e4_1123">27</span><span  className="e4_1124">28</span><span  className="e4_1125">1</span><span  className="e4_1126">2</span><span  className="e4_1127">3</span><span  className="e4_1128">4</span>
+      </div>
+      <div className=e4_1129>
+        <div className="e4_1130"></div>
+        <div className=e4_1131>
+          <div className="e4_1132"></div>
+          <div className="e4_1133"></div>
+          <div className="e4_1134"></div>
+          <div className="e4_1135"></div>
+          <div className="e4_1136"></div>
+          <div className="e4_1137"></div>
+          <div className="e4_1138"></div>
+        </div>
+        <div className="e4_1139"></div><span  className="e4_1140">5</span><span  className="e4_1141">6</span><span  className="e4_1142">7</span><span  className="e4_1143">8</span><span  className="e4_1144">9</span><span  className="e4_1145">10</span><span  className="e4_1146">11</span>
+      </div>
+      <div className=e4_1147>
+        <div className="e4_1148"></div>
+        <div className=e4_1149>
+          <div className="e4_1150"></div>
+          <div className="e4_1151"></div>
+          <div className="e4_1152"></div>
+          <div className="e4_1153"></div>
+          <div className="e4_1154"></div>
+          <div className="e4_1155"></div>
+          <div className="e4_1156"></div>
+        </div>
+        <div className=e4_1157>
+          <div className="e4_1158"></div>
+          <div className="e4_1159"></div>
+          <div className="e4_1160"></div>
+          <div className="e4_1161"></div>
+          <div className="e4_1162"></div>
+          <div className="e4_1163"></div>
+        </div>
+        <div className="e4_1164"></div><span  className="e4_1165">12</span><span  className="e4_1166">13</span><span  className="e4_1167">14</span><span  className="e4_1168">15</span><span  className="e4_1169">16</span><span  className="e4_1170">17</span><span  className="e4_1171">18</span>
+      </div>
+      <div className=e4_1172>
+        <div className="e4_1173"></div>
+        <div className=e4_1174>
+          <div className="e4_1175"></div>
+          <div className="e4_1176"></div>
+          <div className="e4_1177"></div>
+          <div className="e4_1178"></div>
+          <div className="e4_1179"></div>
+          <div className="e4_1180"></div>
+          <div className="e4_1181"></div>
+        </div>
+        <div className=e4_1182>
+          <div className="e4_1183"></div>
+          <div className="e4_1184"></div>
+          <div className="e4_1185"></div>
+          <div className="e4_1186"></div>
+          <div className="e4_1187"></div>
+          <div className="e4_1188"></div>
+          <div className="e4_1189"></div>
+        </div>
+        <div className="e4_1190"></div><span  className="e4_1191">19</span><span  className="e4_1192">20</span><span  className="e4_1193">21</span><span  className="e4_1194">22</span><span  className="e4_1195">23</span><span  className="e4_1196">24</span><span  className="e4_1197">25</span>
+      </div>
+      <div className=e4_1198>
+        <div className="e4_1199"></div>
+        <div className=e4_1200>
+          <div className="e4_1201"></div>
+          <div className="e4_1202"></div>
+          <div className="e4_1203"></div>
+          <div className="e4_1204"></div>
+          <div className="e4_1205"></div>
+          <div className="e4_1206"></div>
+          <div className="e4_1207"></div>
+        </div>
+        <div className=e4_1208>
+          <div className="e4_1209"></div>
+          <div className="e4_1210"></div>
+          <div className="e4_1211"></div>
+          <div className="e4_1212"></div>
+          <div className="e4_1213"></div>
+          <div className="e4_1214"></div>
+        </div>
+        <div className="e4_1215"></div><span  className="e4_1216">26</span><span  className="e4_1217">27</span><span  className="e4_1218">28</span><span  className="e4_1219">29</span><span  className="e4_1220">30</span><span  className="e4_1221">31</span><span  className="e4_1222">1</span>
+      </div>
+    </div>
+    <div className=e4_1223><span  className="e4_1224">2023.03</span>
+      <div className=e4_1225>
+        <div className=e4_1226>
+          <div className="e4_1227"></div>
+          <div className="e4_1228"></div>
+        </div>
+      </div>
+      <div className=e4_1229>
+        <div className=e4_1230>
+          <div className="e4_1231"></div>
+          <div className="e4_1232"></div>
+        </div>
+      </div>
+    </div>
+    <div className=e4_1233>
+      <div className=e4_1234>
+        <div className="e4_1235"></div><span  className="e4_1236">예약가능</span>
+      </div>
+      <div className=e4_1237>
+        <div className="e4_1238"></div><span  className="e4_1239">날짜선택</span>
+      </div>
+    </div>
+  </div> */}
+  {/* <div className=e4_1240>
+    <div className="e4_1241"></div>
+    <div className="e4_1242"></div>
+    <div className="e4_1243"></div><span  className="e4_1244">예약 현황 확인</span>
+    <div className="e4_1245"></div>
+    <div className=e4_1246>
+      <div className=e4_1247>
+        <div className=e4_1248>
+          <div className=e4_1249>
+            <div className="e4_1250"></div><span  className="e4_1251">13:00 ~ 14:00</span><span  className="e4_1252">예약가능</span>
+            <div className=e4_1253>
+              <div className=e4_1254>
+                <div className="e4_1255"></div>
+                <div className="e4_1256"></div>
+              </div>
+            </div>
+          </div>
+          <div className=e4_1257>
+            <div className="e4_1258"></div><span  className="e4_1259">15:00 ~ 16:00</span><span  className="e4_1260">예약가능</span>
+            <div className=e4_1261>
+              <div className=e4_1262>
+                <div className="e4_1263"></div>
+                <div className="e4_1264"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className=e4_1265>
+      <div className="e4_1266"></div><span  className="e4_1267">05:00 ~ 06:00</span><span  className="e4_1268">예약불가</span>
+      <div className=e4_1269>
+        <div className=e4_1270>
+          <div className="e4_1271"></div>
+          <div className="e4_1272"></div>
+        </div>
+      </div>
+    </div>
+    <div className=e4_1273>
+      <div className="e4_1274"></div><span  className="e4_1275">06:00 ~ 07:00</span><span  className="e4_1276">예약가능</span>
+      <div className=e4_1277>
+        <div className=e4_1278>
+          <div className="e4_1279"></div>
+          <div className="e4_1280"></div>
+        </div>
+      </div>
+    </div>
+    <div className=e4_1281>
+      <div className="e4_1282"></div><span  className="e4_1283">07:00 ~ 08:00</span><span  className="e4_1284">예약가능</span>
+      <div className=e4_1285>
+        <div className=e4_1286>
+          <div className="e4_1287"></div>
+          <div className="e4_1288"></div>
+        </div>
+      </div>
+    </div>
+    <div className=e4_1289>
+      <div className="e4_1290"></div><span  className="e4_1291">08:00 ~ 09:00</span><span  className="e4_1292">예약가능</span>
+      <div className=e4_1293>
+        <div className=e4_1294>
+          <div className="e4_1295"></div>
+          <div className="e4_1296"></div>
+        </div>
+      </div>
+    </div>
+    <div className=e4_1297>
+      <div className="e4_1298"></div><span  className="e4_1299">09:00 ~ 10:00</span><span  className="e4_1300">예약가능</span>
+      <div className=e4_1301>
+        <div className=e4_1302>
+          <div className="e4_1303"></div>
+          <div className="e4_1304"></div>
+        </div>
+      </div>
+    </div>
+    <div className=e4_1305>
+      <div className="e4_1306"></div><span  className="e4_1307">10:00 ~ 11:00</span><span  className="e4_1308">예약가능</span>
+      <div className=e4_1309>
+        <div className=e4_1310>
+          <div className="e4_1311"></div>
+          <div className="e4_1312"></div>
+        </div>
+      </div>
+    </div>
+    <div className=e4_1313>
+      <div className="e4_1314"></div><span  className="e4_1315">11:00 ~ 12:00</span><span  className="e4_1316">예약가능</span>
+      <div className=e4_1317>
+        <div className=e4_1318>
+          <div className="e4_1319"></div>
+          <div className="e4_1320"></div>
+        </div>
+      </div>
+    </div>
+    <div className=e4_1321>
+      <div className="e4_1322"></div><span  className="e4_1323">12:00 ~ 13:00</span><span  className="e4_1324">예약가능</span>
+      <div className=e4_1325>
+        <div className=e4_1326>
+          <div className="e4_1327"></div>
+          <div className="e4_1328"></div>
+        </div>
+      </div>
+    </div>
+  </div> */}
+  {/* <div className=e4_1329>
+    <div className="e4_1330"></div>
+    <div className=e4_1331><span  className="e4_1332">예약하기</span>
+      <div className=e4_1333>
+        <div className=e4_1334>
+          <div className="e4_1335"></div>
+          <div className="e4_1336"></div>
+        </div>
+      </div>
+    </div>
+  </div> */}
+</div>
     </div>
   );
 }
