c0f2f7f08768e74534c878d826512cedf08c65c1
dlwnstn0524 <ljs052450@gmail.com>
2023-03-22 21:04:16 +0900

    [FEAT] BigGround Reservation API

--- balland/pages/api/reservation/biggroundreservation.ts
+++ balland/pages/api/reservation/biggroundreservation.ts
@@ -4,8 +4,13 @@ import clientPromise from '../../../lib/mongodb';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const client = await clientPromise;
   if (req.method === 'POST') {
+    const info  = req.body;
+    console.log();
     const db = client.db('balland');
-    const readdatereservation = await db.collection('reservationperdate').findOne();
+    const reservationInfo = await db.collection('reservationInfo').insertOne(info);
+    // const readdatereservation = await db.collection('reservationperdate').findOne();
+    res.status(200)
+    res.send("Success!")
   } else {
     res.status(500).json({ result: false, error: 'Route not valid' });
   }
