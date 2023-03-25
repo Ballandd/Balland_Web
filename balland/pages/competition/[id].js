0317acf85aeaff423535761156e4cc934af22e9c
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2023-03-25 21:44:29 +0900

    feat: Competition 메인 페이지 구성, 대회 별 다이나믹 라우팅 구성

---
+++ balland/pages/competition/[id].js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useRouter } from 'next/router'
+
+const CompetitionDetail = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  return (
+    <div>
+      대회 번호 : {id}
+    </div>
+  )
+}
+export default CompetitionDetail
--- balland/pages/competition/index.js
+++ balland/pages/competition/index.js
@@ -1,287 +1,34 @@
 import Head from "next/head";
 import styles from "../../styles/Competition.module.css"
 import CompetitionInformation from "../../components/CompetitionInformation.tsx"
+import posts from "../competition.json"
+import Link from "next/link";
 export default function Competition() {
-
+  const producs = posts
+  console.log(producs[1].id %2)
   return (
     <div>
       <Head>
-        <title>Tailwind Navbar Tutorial</title>
+        <title>대회 정보</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <div className = "w-screen h-96">
-      <CompetitionInformation
-        picture = "/BallandLogo.png"
-        name = "AFC배 축구대회"
-        period = "2023.03.31~2023.04.12"
-        part = "아주대학교 축구 동아리 AFC"
-        prize = "20만원"
-      />
-    </div>
-      
-      {/* content goes here */}
-      {/* <div className={styles.e12_748}>
-  <div className={styles.e12_749}>
-    <div className={styles.e12_750}></div>
-    <div className={styles.e12_751}></div>
-    <div className={styles.e12_752}></div><span className={styles.e12_753}>진행중</span>
-    <span className={styles.e12_754}>KUSF 대학야구 U-리그 A조</span>
-    <div className={styles.e12_755}></div>
-    <div className={styles.e12_756}>
-      <div className={styles.e12_757}><span  className={styles.e12_758}>대회기간 : 23.03.23 ~ 23.06.23</span>
-        <div className={styles.e12_759}>
-          <div className={styles.e12_760}>
-            <div className={styles.e12_761}></div>
-            <div className={styles.e12_762}></div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.e12_763}><span className={styles.e12_764}>대회주체 : 한국대학스포츠협의회</span>
-        <div className={styles.e12_765}>
-          <div className={styles.e12_766}>
-            <div className={styles.e12_767}></div>
-            <div className={styles.e12_768}></div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.e12_769}><span className={styles.e12_770}>우승상금 : 300만원</span>
-        <div className={styles.e12_771}>
-          <div className={styles.e12_772}>
-            <div className={styles.e12_773}></div>
-            <div className={styles.e12_774}></div>
-          </div>
-        </div>
+      <div className = "w-full h-full left-1/2 top-1/2 ">
+        <div className = "grid gap-4 grid-flow-row grid-cols-2">
+            {producs.map((compete)=>(
+              <div className = {`${compete.id % 2 == 0 ?'grid justify-items-start':'grid justify-items-end'}`}>
+                <Link href = {`/competition/${compete.id}`}>
+              <CompetitionInformation
+                picture = {compete.picture}
+                name = {compete.name}
+                period = {compete.period}
+                part = {compete.part}
+                prize = {compete.prize}
+                />
+                </Link>
+                </div>
+            ))}
       </div>
     </div>
-  </div>
-  <div className={styles.e12_775}>
-     <div className={styles.e12_776}></div>
-     <div className={styles.e12_777}></div>
-     <div className={styles.e12_778}></div><span  className={styles.e12_779}>진행중</span>
-     <span  className={styles.e12_780}>KUSF 대학야구 U-리그 A조</span>
-     <div className={styles.e12_781}></div>
-     <div className={styles.e12_782}>
-       <div className={styles.e12_783}><span  className={styles.e12_784}>대회기간 : 23.03.23 ~ 23.06.23</span>
-         <div className={styles.e12_785}>
-           <div className={styles.e12_786}>
-             <div className={styles.e12_787}></div>
-             <div className={styles.e12_788}></div>
-           </div>
-         </div>
-       </div>
-       <div className={styles.e12_789}><span  className={styles.e12_790}>대회주체 : 한국대학스포츠협의회</span>
-         <div className={styles.e12_791}>
-           <div className={styles.e12_792}>
-             <div className={styles.e12_793}></div>
-             <div className={styles.e12_794}></div>
-           </div>
-         </div>
-       </div>
-       <div className={styles.e12_795}><span  className="e12_796">우승상금 : 300만원</span>
-         <div className={styles.e12_797}>
-           <div className={styles.e12_798}>
-             <div className={styles.e12_799}></div>
-             <div className={styles.e12_800}></div>
-           </div>
-         </div>
-       </div>
-     </div>
-   </div>
-   <div className={styles.e12_801}>
-     <div className={styles.e12_802}></div>
-     <div className={styles.e12_803}></div>
-     <div className={styles.e12_804}></div><span  className={styles.e12_805}>진행중</span>
-     <span  className={styles.e12_806}>KUSF 대학야구 U-리그 A조</span>
-     <div className={styles.e12_807}></div>
-     <div className={styles.e12_808}>
-       <div className={styles.e12_809}><span  className={styles.e12_810}>대회기간 : 23.03.23 ~ 23.06.23</span>
-         <div className={styles.e12_811}>
-           <div className={styles.e12_812}>
-             <div className="e12_813"></div>
-             <div className="e12_814"></div>
-           </div>
-         </div>
-       </div>
-       <div className={styles.e12_815}><span  className={styles.e12_816}>대회주체 : 한국대학스포츠협의회</span>
-         <div className={styles.e12_817}>
-           <div className={styles.e12_818}>
-             <div className={styles.e12_819}></div>
-             <div className={styles.e12_820}></div>
-           </div>
-         </div>
-       </div>
-       <div className={styles.e12_821}><span  className={styles.e12_822}>우승상금 : 300만원</span>
-         <div className={styles.e12_823}>
-           <div className={styles.e12_824}>
-             <div className={styles.e12_825}></div>
-             <div className={styles.e12_826}></div>
-           </div>
-         </div>
-       </div>
-     </div>
-   </div>
-   <div className={styles.e12_827}>
-     <div className={styles.e12_828}></div>
-     <div className={styles.e12_829}></div>
-     <div className={styles.e12_830}></div><span  className={styles.e12_831}>종료</span>
-     <span  className={styles.e12_832}>KUSF 대학야구 U-리그 A조</span>
-     <div className={styles.e12_833}></div>
-     <div className={styles.e12_834}>
-       <div className={styles.e12_835}><span  className={styles.e12_836}>대회기간 : 23.03.23 ~ 23.06.23</span>
-         <div className={styles.e12_837}>
-           <div className={styles.e12_838}>
-             <div className={styles.e12_839}></div>
-             <div className={styles.e12_840}></div>
-           </div>
-         </div>
-       </div>
-       <div className={styles.e12_841}><span  className={styles.e12_842}>대회주체 : 한국대학스포츠협의회</span>
-         <div className={styles.e12_843}>
-           <div className={styles.e12_844}>
-             <div className={styles.e12_845}></div>
-             <div className={styles.e12_846}></div>
-           </div>
-         </div>
-       </div>
-       <div className={styles.e12_847}><span  className={styles.e12_848}>우승상금 : 300만원</span>
-         <div className={styles.e12_849}>
-           <div className={styles.e12_850}>
-             <div className={styles.e12_851}></div>
-             <div className={styles.e12_852}></div>
-           </div>
-         </div>
-       </div>
-     </div>
-   </div>
-   <div className=e12_853>
-    <div className="e12_854"></div>
-    <div className="e12_855"></div>
-    <div className="e12_856"></div><span  className="e12_857">진행중</span><span  className="e12_858">KUSF 대학야구 U-리그 A조</span>
-    <div className="e12_859"></div>
-    <div className=e12_860>
-      <div className=e12_861><span  className="e12_862">대회기간 : 23.03.23 ~ 23.06.23</span>
-        <div className=e12_863>
-          <div className=e12_864>
-            <div className="e12_865"></div>
-            <div className="e12_866"></div>
-          </div>
-        </div>
-      </div>
-      <div className=e12_867><span  className="e12_868">대회주체 : 한국대학스포츠협의회</span>
-        <div className=e12_869>
-          <div className=e12_870>
-            <div className="e12_871"></div>
-            <div className="e12_872"></div>
-          </div>
-        </div>
-      </div>
-      <div className=e12_873><span  className="e12_874">우승상금 : 300만원</span>
-        <div className=e12_875>
-          <div className=e12_876>
-            <div className="e12_877"></div>
-            <div className="e12_878"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div className=e12_879>
-    <div className="e12_880"></div>
-    <div className="e12_881"></div>
-    <div className="e12_882"></div><span  className="e12_883">진행중</span><span  className="e12_884">KUSF 대학야구 U-리그 A조</span>
-    <div className="e12_885"></div>
-    <div className=e12_886>
-      <div className=e12_887><span  className="e12_888">대회기간 : 23.03.23 ~ 23.06.23</span>
-        <div className=e12_889>
-          <div className=e12_890>
-            <div className="e12_891"></div>
-            <div className="e12_892"></div>
-          </div>
-        </div>
-      </div>
-      <div className=e12_893><span  className="e12_894">대회주체 : 한국대학스포츠협의회</span>
-        <div className=e12_895>
-          <div className=e12_896>
-            <div className="e12_897"></div>
-            <div className="e12_898"></div>
-          </div>
-        </div>
-      </div>
-      <div className=e12_899><span  className="e12_900">우승상금 : 300만원</span>
-        <div className=e12_901>
-          <div className=e12_902>
-            <div className="e12_903"></div>
-            <div className="e12_904"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div className=e12_905>
-    <div className="e12_906"></div>
-    <div className="e12_907"></div>
-    <div className="e12_908"></div><span  className="e12_909">진행중</span><span  className="e12_910">KUSF 대학야구 U-리그 A조</span>
-    <div className="e12_911"></div>
-    <div className=e12_912>
-      <div className=e12_913><span  className="e12_914">대회기간 : 23.03.23 ~ 23.06.23</span>
-        <div className=e12_915>
-          <div className=e12_916>
-            <div className="e12_917"></div>
-            <div className="e12_918"></div>
-          </div>
-        </div>
-      </div>
-      <div className=e12_919><span  className="e12_920">대회주체 : 한국대학스포츠협의회</span>
-        <div className=e12_921>
-          <div className=e12_922>
-            <div className="e12_923"></div>
-            <div className="e12_924"></div>
-          </div>
-        </div>
-      </div>
-      <div className=e12_925><span  className="e12_926">우승상금 : 300만원</span>
-        <div className=e12_927>
-          <div className=e12_928>
-            <div className="e12_929"></div>
-            <div className="e12_930"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div className=e12_931>
-    <div className="e12_932"></div>
-     <div className="e12_933"></div>
-     <div className="e12_934"></div><span  className="e12_935">종료</span><span  className="e12_936">KUSF 대학야구 U-리그 A조</span>
-     <div className="e12_937"></div>
-     <div className=e12_938>
-       <div className=e12_939><span  className="e12_940">대회기간 : 23.03.23 ~ 23.06.23</span>
-         <div className=e12_941>
-           <div className=e12_942>
-             <div className="e12_943"></div>
-             <div className="e12_944"></div>
-           </div>
-         </div>
-       </div>
-       <div className=e12_945><span  className="e12_946">대회주체 : 한국대학스포츠협의회</span>
-         <div className=e12_947>
-           <div className=e12_948>
-             <div className="e12_949"></div>
-             <div className="e12_950"></div>
-           </div>
-         </div>
-       </div>
-       <div className=e12_951><span  className="e12_952">우승상금 : 300만원</span>
-         <div className=e12_953>
-           <div className=e12_954>
-             <div className="e12_955"></div>
-             <div className="e12_956"></div>
-           </div>
-         </div>
-       </div>
-     </div>
-     </div>
-      </div> */}
     </div>
   );
 }
