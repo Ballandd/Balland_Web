aea643c6f6567be30b04e15e46743357cd2fe5d2
Choi Jaemin <ruka4739@gmail.com>
2023-04-21 00:15:17 +0900

    Style(Footer.js): PC 폰트 사이즈 줄임

--- balland/components/Footer.js
+++ balland/components/Footer.js
@@ -1,10 +1,10 @@
 const Footer = () => {
     return(
         <footer className="mt-6 mb-10 bg-white">
-            <div className="pt-6 pl-4 xxs:pl-6 xs:pl-8 s:pl-10 sm:pl-16 md:pl-20 text-gray-400">
-                <h1 className="text-base mb-1">Balland</h1>
-                <h2 className="text-xs">베타버전 서비스</h2>
-                <h2 className="text-xs">Copyright Balland. All Rights Reserved</h2>
+            <div className="pt-6 pl-6 xs:pl-8 s:pl-10 sm:pl-16 md:pl-20 text-gray-400">
+                <h1 className="text-base md:text-[12px] mb-1 md:mb-px">Balland</h1>
+                <h2 className="text-xs md:text-[8px]">베타버전 서비스</h2>
+                <h2 className="text-xs md:text-[8px]">Copyright Balland. All Rights Reserved</h2>
             </div>
         </footer>
     )
