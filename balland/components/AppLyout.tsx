987e1c7f06f5b534fded13609e19003dfc9d1241
Choi Jaemin <ruka4739@gmail.com>
2023-04-20 23:02:33 +0900

    Feat(footer.js): footer 추가

--- balland/components/AppLyout.tsx
+++ balland/components/AppLyout.tsx
@@ -1,4 +1,5 @@
 import NavBar from "./Navbar.js"
+import Footer from "./Footer.js"
 import React, { useEffect, useState } from "react"
 interface Props {
   children: React.ReactNode
@@ -11,6 +12,7 @@ export default function Layout({ children }: Props) {
       <div className="mx-5 xxs:mx-7 xs:mx-8 s:mx-15 sm:mx-30 md:mx-36 w-100% h-100% mt-12 left-1/2 top-1/2">
         {children}
       </div>
+      <Footer />
     </>
   )
 }
---
+++ balland/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return(
+        <footer className="mt-6 mb-10 bg-white">
+            <div className="pt-6 pl-20 text-gray-500">
+                <h1 className="text-lg mb-1">Balland</h1>
+                <h2 className="text-sm">베타버전 서비스</h2>
+                <h2 className="text-sm">Copyright Balland. All Rights Reserved</h2>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
