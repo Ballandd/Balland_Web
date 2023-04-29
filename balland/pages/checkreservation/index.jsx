d37026df3b3598eb87049865805470c6083825a8
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2023-04-29 16:10:36 +0900

    Feat: 나의 예약 현황 페이지 세팅

--- balland/pages/checkreservation/index.jsx
+++ balland/pages/checkreservation/index.jsx
@@ -10,15 +10,7 @@ export default function Reservation() {
       </Head>
 
       {/* content goes here */}
-      <div className="py-32 text-center">
-        <div className="text-4xl font-extrabold">reservation page </div>
-      </div>
-      <div className="flex justify-center">
-        <Link href="/reservation/bigground">대운동장 예약</Link>
-      </div>
-      <div className="flex justify-center mt-20">
-        <Link href="/reservation/smallground">소운동장 예약</Link>
-      </div>
+      
     </div>
   )
 }
