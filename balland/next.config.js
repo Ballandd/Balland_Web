6d0470728ce4d5fce135188fcc72f973b0779ddb
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2023-04-29 17:27:16 +0900

    Chore: pageExtensions 추가

--- balland/next.config.js
+++ balland/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
   images: {
     domains: ["localhost", "*","drive.google.com"],
   },
