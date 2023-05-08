6746444e823b0055972b96470523e443fcdebfbb
yoonui <yoony0612@ajou.ac.kr>
2023-05-08 22:22:12 +0900

    ADD: 상단 tab 구현 중

--- balland/components/CompetitionInformation.tsx
+++ balland/components/CompetitionInformation.tsx
@@ -11,38 +11,55 @@ export interface CompetitionInformationProps {
 
 const CompetitionInformation = (props: CompetitionInformationProps) => {
   const isStatus = props.status
-  const prize_unit = props.prize.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  const prize_unit = props.prize
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 
   return (
     <div className="sticky xs:w-[300px] s:w-[350px] h-[140px] xxs:h-[140px] xs:h-[150px] s:h-[190px] rounded border-2 bg-white">
       <div className="mt-5 ml-5 mr-5">
         <div className="flex flex-row">
-          <div className="w-32 flex flex-col">
-            <img className="h-20 w-20 xs:h-20 xs:w-20 s:h-28 s:w-28 m-auto" src={`${process.env.IMAGE_URL}/Logo/${props.picture}.png`} />
+          <div className="flex flex-col w-32">
+            <img
+              className="w-20 h-20 m-auto xs:h-20 xs:w-20 s:h-28 s:w-28"
+              src={`${process.env.IMAGE_URL}/Logo/${props.picture}.png`}
+            />
             {isStatus === 2 ? (
-              <div className="xxs:mt-1 xs:mt-3 border rounded border-sky-600">
-                <h2 className="text-center text-sky-700 text-[10px] xxs:text-xs xs:text-base s:text-lg">진행중</h2>
+              <div className="border rounded xxs:mt-1 xs:mt-3 border-sky-600">
+                <h2 className="text-center text-sky-700 text-[10px] xxs:text-xs xs:text-base s:text-lg">
+                  진행중
+                </h2>
               </div>
             ) : isStatus === 3 ? (
-              <div className="xxs:mt-1 xs:mt-3 border rounded border-red-600">
-                <h2 className="text-center text-red-600 text-[10px] xxs:text-xs xs:text-base s:text-lg">진행완료</h2>
+              <div className="border border-red-600 rounded xxs:mt-1 xs:mt-3">
+                <h2 className="text-center text-red-600 text-[10px] xxs:text-xs xs:text-base s:text-lg">
+                  진행완료
+                </h2>
               </div>
             ) : (
-              <div className="xxs:mt-1 xs:mt-3 border rounded border-green-600">
-                <h2 className="text-center text-green-600 text-[10px] xxs:text-xs xs:text-base s:text-lg">진행예정</h2>
+              <div className="border border-green-600 rounded xxs:mt-1 xs:mt-3">
+                <h2 className="text-center text-green-600 text-[10px] xxs:text-xs xs:text-base s:text-lg">
+                  진행예정
+                </h2>
               </div>
             )}
           </div>
           <div>
-            <div className="divide-y-2 divide-solid divide-black">
-              <h2 className=" ml-5 left-9 top-5 font-mono text-left text-xs xxs:text-sm xs:text-base s:text-lg font-bold">
+            <div className="divide-y-2 divide-black divide-solid">
+              <h2 className="ml-5 font-mono text-xs font-bold text-left left-9 top-5 xxs:text-sm xs:text-base s:text-lg">
                 {props.name}
               </h2>
-              <div className="h-full flex flex-col mt-1 ml-5 ">
+              <div className="flex flex-col h-full mt-1 ml-5 ">
                 <div className="flex flex-row mt-1 xxs:mt-2 s:mt-3 left-9 top-5 ">
                   <Icon icon="material-symbols:calendar-today" width="15" />
                   <h2 className="ml-2 font-mono text-left text-[4px] xxs:text-[6px] xs:text-[6px] s:text-xs">
-                    {props.period.slice(5,7)+"/"+props.period.slice(8,10)+" ~ "+props.period.slice(18,20)+"/"+props.period.slice(21,23)}
+                    {props.period.slice(5, 7) +
+                      "/" +
+                      props.period.slice(8, 10) +
+                      " ~ " +
+                      props.period.slice(18, 20) +
+                      "/" +
+                      props.period.slice(21, 23)}
                   </h2>
                 </div>
                 <div className="flex flex-row mt-1 xxs:mt-2 s:mt-3 left-9 top-5 ">
--- balland/pages/checkreservation/index.jsx
+++ balland/pages/checkreservation/index.jsx
@@ -1,18 +1,44 @@
 import Head from "next/head"
 import Link from "next/link"
 import WaitingService from "../../components/WaitingService.tsx"
-
+import { useState } from "react"
 
 export default function Reservation() {
+  const [currentTab, setCurrentTab] = useState(0)
+
+  const tabMenu = [
+    { title: "tab1", content: <div>tab1</div> },
+    { title: "tab2", content: <div>tab2</div> },
+    { title: "tab3", content: <div>tab3</div> },
+  ]
   return (
     <div>
       <Head>
         <title>Reservation Main</title>
         <link rel="icon" href="/AU.png" />
       </Head>
-      <div className = "h-screen">
-        <WaitingService />
+      <div className="h-screen">
+        <div className="divide-y-2 divide-black divide-solid">
+          {tabMenu.map((item, index) => {
+            return (
+              <button
+                key={index}
+                className={`${
+                  currentTab === index ? "text-red-900" : ""
+                } mr-10`}
+                onClick={() => {
+                  setCurrentTab(index)
+                }}
+              >
+                {item.title}
+              </button>
+            )
+          })}
+        </div>
+        <div>{tabMenu[currentTab].content}</div>
       </div>
     </div>
   )
 }
+
+//
