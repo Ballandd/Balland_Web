de21e8118eeffb7728bb2e43fd3b3eee228583b3
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2023-05-17 21:12:01 +0900

    Fix: myfuturereservation 구조 수정

--- balland/pages/api/reservation/myfuturereservation.ts
+++ balland/pages/api/reservation/myfuturereservation.ts
@@ -1,23 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import clientPromise from "../../../lib/mongodb";
+import connect from "../../../lib/config.js";
+import reservationinfo from "../../../lib/model/reservationinfo.js";
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse,
 ) {
-    const client = await clientPromise;
-    const db = client.db("balland")
     if(req.method === "POST"){
-        const {email} = req.body.body;
+        connect()
+        const email = req.body.body.email;
         const today = new Date();
-        const myReservation = await db.collection("reservationinfo").find({email}).toArray();
-        const responseReservation = myReservation.filter((reservation)=>{
-            const reservationDate = new Date(reservation.reservationDate)
-                return reservationDate >= today
-        })
-        res.status(200).json({
-            type : true,
-            data : responseReservation,
+        const findUserReservation = {email : email}
+        reservationinfo.find(findUserReservation).then(function(obj){
+            const filterData = obj.filter((reservation)=> {
+                const reservationDate = new Date(reservation.reservationDate)
+                    return reservationDate >= today
+            })
+            res.json({
+                type : true,
+                data : filterData
+            })
         })
     }
 }
