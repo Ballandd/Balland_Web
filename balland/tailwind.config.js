850179da6451a34a6351c041095b906b6333a998
yoonui <yoony0612@ajou.ac.kr>
2023-05-31 22:28:18 +0900

    FIX: merge

--- balland/tailwind.config.js
+++ balland/tailwind.config.js
@@ -12,30 +12,13 @@ module.exports = {
       table: ["Helvetica", "Arial", "sans-serif"],
     },
     extend: {
-<<<<<<< HEAD
       fontFamily: {
         sans: ["AjouTTF"],
         mono: ["AjouTTF"],
       },
-    },
-    screens: {
-      xxs: "320px",
-      xs: "360px",
-      s: "480px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-=======
-      fontFamily:{
-        sans : ["AjouTTF"],
-        mono :  ["AjouTTF"]
-      },
       fontSize: {
-        '2xs': '0.625rem', // 2px
-      }
->>>>>>> 6d6a910301f6e1f5c3e69f006593ed4715c6eb61
+        "2xs": "0.625rem", // 2px
+      },
     },
   },
   plugins: [require("tailwind-scrollbar-hide")],
